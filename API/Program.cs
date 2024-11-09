5f3499ec261ee991f3dc30b18e3104686c4fc18b
Mahomed <mahomedprogramador@programador.com>
2024-11-09 20:27:11 +0000

    Angular client side configured

--- API/Program.cs
+++ API/Program.cs
@@ -5,14 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(
-    options => options.UseSqlite(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    )
-);
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
