31100a90823837927343b0354f8bc3d99f82c94d
Mahomed <mahomedprogramador@programador.com>
2024-12-13 17:22:12 +0000

    Upload photo Functionality

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/members';
-import { RtlScrollAxisType } from '@angular/cdk/platform';
 import { of, tap } from 'rxjs';
+import { Photo } from '../_models/photos';
 
 
 @Injectable({
@@ -34,5 +34,31 @@ export class MembersService {
           this.members.update(members => members.map(m => m.username === member.username ? member : m))
       })
     );
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      tap(() => {
+          this.members.update(members => members.map(m => {
+            if (m.photos.includes(photo)){
+              m.photoUrl = photo.url;
+            }
+            return m;
+          }))
+      })
+    );
+  }
+
+  deletePhoto(photo: Photo) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
+      tap(() => {
+          this.members.update(members => members.map(m => {
+            if (m.photos.includes(photo)) {
+              m.photos = m.photos.filter(x => x.id !== photo.id);
+            }           
+            return m;
+          }))
+      })
+    )
   }
 }
